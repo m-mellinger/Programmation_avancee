0d7e758d1b7901d8da11bc391b999dcd16d418fd
tac-univLorraine <kevin.engrand7@gmail.com>
2019-10-07 19:43:41 +0200

    Main

---
+++ Main.c
@@ -0,0 +1,51 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+
+int main(int argc, char *argv[])
+{
+  SDL_Window* fenetre; // Déclaration de la fenêtre
+  SDL_Event evenements; // Événements liés à la fenêtre
+  bool terminer = false;
+  if(SDL_Init(SDL_INIT_VIDEO) < 0) // Initialisation de la SDL
+    {
+      printf("Erreur d’initialisation de la SDL: %s",SDL_GetError());
+      SDL_Quit();
+      return EXIT_FAILURE;
+    }
+  // Créer la fenêtre
+  fenetre = SDL_CreateWindow("Fenetre SDL", SDL_WINDOWPOS_CENTERED,
+			     SDL_WINDOWPOS_CENTERED,SCREEN_WIDTH ,SCREEN_HEIGHT, SDL_WINDOW_RESIZABLE);
+  if(fenetre == NULL) // En cas d’erreur
+    {
+      printf("Erreur de la creation d’une fenetre: %s",SDL_GetError());
+      SDL_Quit();
+      return EXIT_FAILURE;
+    }
+  // Boucle principale
+  while(!terminer)
+    {
+      while( SDL_PollEvent( &evenements ) )
+	switch(evenements.type)
+	  {
+	  case SDL_QUIT:
+	    terminer = true; break;
+	  case SDL_KEYDOWN:
+	    switch(evenements.key.keysym.sym)
+	      {
+	      case SDLK_ESCAPE:
+	      case SDLK_q:
+		terminer = true; break;
+	      }
+	  }
+    }
+  // Quitter SDL
+  SDL_DestroyWindow(fenetre);
+  SDL_Quit();
+  return 0;
+}
